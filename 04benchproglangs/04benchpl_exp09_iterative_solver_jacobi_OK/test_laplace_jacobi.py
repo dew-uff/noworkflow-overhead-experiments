69818effff77f683704b980eaa9200813372c145
JoaoLopez <joao.p.lopez01@gmail.com>
2025-12-12 19:39:55 +0000

    Adding Iterative Solver Jacobi experiment

--- 04benchproglangs/04benchpl_exp09_iterative_solver_jacobi_OK/test_laplace_jacobi.py
+++ 04benchproglangs/04benchpl_exp09_iterative_solver_jacobi_OK/test_laplace_jacobi.py
@@ -1,75 +1,116 @@
 import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parent / 'speedupy'))
 import numpy as np
 import time
-import copy
 
-def loop_time_step(u, func_globals=None):
+def loop_time_step(u):
     """
         Take a time step in the desired numerical solution 
         u found using loops
     """
-    aux = copy.deepcopy(u)
-    (n, m) = aux.shape
-    error = 0.0
-    for i in range(1, n - 1):
-        for j in range(1, m - 1):
-            temp = aux[i, j]
-            aux[i, j] = ((aux[i - 1, j] + aux[i + 1, j] + aux[i, j - 1] + aux[i, j + 1]) * 4.0 + aux[i - 1, j - 1] + aux[i - 1, j + 1] + aux[i + 1, j - 1] + aux[i + 1, j + 1]) / 20.0
-            difference = aux[i, j] - temp
-            error += difference * difference
-    return (aux, np.sqrt(error))
+    n, m = u.shape
 
-def loop_solver(n, func_globals=None):
+    error = 0.0
+    for i in range(1, n-1):
+        for j in range(1, m-1):
+            temp = u[i, j]
+            u[i, j] = (
+                       (u[i-1, j] + u[i+1, j] + u[i, j-1] + u[i, j+1])*4.0 +
+                       u[i-1, j-1] + u[i-1, j+1] + u[i+1, j-1] +
+                       u[i+1, j+1]
+            )/20.0
+
+            difference = u[i, j] - temp
+            error += difference*difference
+
+    return u, np.sqrt(error)
+
+#----------------------
+# Function: loop_solver
+#----------------------
+
+def loop_solver(n):
     """
         Find the desired numerical solution using loops
     """
+
     j = complex(0, 1)
     pi_c = np.pi
+
+    # Set the initial condition
     u = np.zeros((n, n), dtype=float)
-    x = np.r_[0.0:pi_c:n * j]
+
+    #       Set the boundary condition
+    x = np.r_[0.0:pi_c:n*j]
     u[0, :] = np.sin(x)
-    u[n - 1, :] = np.sin(x) * np.exp(-pi_c)
+    u[n-1, :] = np.sin(x)*np.exp(-pi_c)
+
     iteration = 0
     error = 2
-    while iteration < 1000000 and error > 1e-10:
-        (u, error) = loop_time_step(u, func_globals=globals())
+    while(iteration < 100000 and error > 1e-10):
+        (u, error) = loop_time_step(u)
         iteration += 1
     return (u, error, iteration)
 
-def vector_time_step(u, func_globals=None):
+#---------------------------
+# Function: vector_time_step
+#---------------------------
+def vector_time_step(u):
     """
         Take a time step in the desired numerical solution v 
         found using vectorization
     """
-    aux = copy.deepcopy(u)
-    u_old = aux.copy()
-    aux[1:-1, 1:-1] = ((aux[0:-2, 1:-1] + aux[2:, 1:-1] + aux[1:-1, 0:-2] + aux[1:-1, 2:]) * 4.0 + aux[0:-2, 0:-2] + aux[0:-2, 2:] + aux[2:, 0:-2] + aux[2:, 2:]) / 20.0
-    return (aux, np.linalg.norm(aux - u_old))
+    u_old = u.copy()
+    u[1:-1, 1:-1] = (
+                     (u[0:-2, 1:-1] + u[2:, 1:-1] + u[1:-1, 0:-2] +
+                      u[1:-1, 2:])*4.0 +
+                     u[0:-2, 0:-2] + u[0:-2, 2:] + u[2:, 0:-2] + u[2:, 2:]
+    )/20.0
 
-def vectorized_solver(n, func_globals=None):
+    return u, np.linalg.norm(u-u_old)
+
+#----------------------------
+# Function: vectorized_solver
+#----------------------------
+def vectorized_solver(n):
     """
         Find the desired numerical solution using vectorization
     """
     j = complex(0, 1)
     pi_c = np.pi
+
+    # Set the initial condition
     u = np.zeros((n, n), dtype=float)
-    x = np.r_[0.0:pi_c:n * j]
+
+    # Set the boundary condition
+    x = np.r_[0.0:pi_c:n*j]
     u[0, :] = np.sin(x)
-    u[n - 1, :] = np.sin(x) * np.exp(-pi_c)
+    u[n-1, :] = np.sin(x)*np.exp(-pi_c)
+
     iteration = 0
     error = 2
-    while iteration < 1000000 and error > 1e-10:
-        (u, error) = vector_time_step(u, func_globals=globals())
+    while(iteration < 100000 and error > 1e-10):
+        (u, error) = vector_time_step(u)
         iteration += 1
     return (u, error, iteration)
 
+
+# number of grid points
+# def main0(num_points):
+    
+# def main1(num_points):
+    
 def main():
     num_points = int(sys.argv[1])
+    #print('LOOP')
     dti = time.perf_counter()
-    (u, error, iteration) = loop_solver(num_points, func_globals=globals())
-    (u, error, iteration) = vectorized_solver(num_points, func_globals=globals())
+    #main0(num_points)
+    (u, error, iteration) = loop_solver(num_points)
+    # print(time.perf_counter() - dti)
+    #print('VECTOR')
+    # dti = time.perf_counter()
+    #main1(num_points)
+    (u, error, iteration) = vectorized_solver(num_points)
     print(time.perf_counter() - dti)
+
 if __name__ == '__main__':
     main()
